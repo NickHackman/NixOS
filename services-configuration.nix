d2c6fb7109eca84b1c388c49fde41d33df25957b
Nick Hackman <snickhackman@gmail.com>
2020-09-07 22:39:17 -0400

    Remove DHCP service when using NetworkManager
    Massively increased boot times, due to it timing out.

--- services-configuration.nix
+++ services-configuration.nix
@@ -58,13 +58,6 @@ in {
 
   networking = {
     hostName = "nixos";
-
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    useDHCP = false;
-    interfaces.wlp2s0.useDHCP = true;
-
     networkmanager.enable = true;
   };
 
