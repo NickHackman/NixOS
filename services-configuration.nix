a7837583f35c0dbbc1c1092cb232a6584eb9e85a
Nick Hackman <snickhackman@gmail.com>
2020-10-14 15:43:29 -0400

    Lower NixOS entries retention
    `/` partition is starting to fillup and is getting closer to 100%
    capacity, which is surprising, but is probably a result of having both
    unstable and stable of Nixpkgs installed.

--- services-configuration.nix
+++ services-configuration.nix
@@ -77,7 +77,7 @@
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 14d";
+    options = "--delete-older-than 7d";
   };
 
   powerManagement.powertop.enable = true;
